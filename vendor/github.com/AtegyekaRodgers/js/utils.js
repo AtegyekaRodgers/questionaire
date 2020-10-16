168269b217333edf771657c68cf4a1a0cdf18591
A Rodgers <rodgers@localhost.localdomain>
2020-10-16 18:56:20 +0300

    fixed the empty wmo issue

---
+++ vendor/github.com/AtegyekaRodgers/js/utils.js
@@ -0,0 +1,7 @@
+module.exports.typedArrayToString = (typedArr)=>{ 
+    var aString = '';
+    for (var i in typedArr) {
+      aString += String.fromCharCode(typedArr[i]);
+    }
+    return aString;
+}
---
+++ vendor/github.com/AtegyekaRodgers/utils.go
@@ -0,0 +1,14 @@
+package websocketMessageObject
+
+import (
+//"fmt"
+)
+/*
+funct typedArrayToString(typedArr) string { 
+    var aString = '';
+    for (var i in typedArr) {
+      aString += String.fromCharCode(typedArr[i]);
+    }
+    return aString;
+}
+*/
