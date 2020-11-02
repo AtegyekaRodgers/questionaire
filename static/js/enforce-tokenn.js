efceb7acff6ecd2e880d9e5a9b99e717ebc7fcd1
Eng. Rodgers Ategyeka <atrodgers77@gmail.com>
2020-11-02 22:23:50 +0300

    changed the tokens to memorable format

--- static/js/enforce-tokenn.js
+++ static/js/enforce-tokenn.js
@@ -5,21 +5,21 @@ function enforceToken(id, serverport){
     var targetElementId = id;
     if(targetElementId=="proceed-as-regulator"){
         var tokenx = prompt("Please enter regulator access token: \nHint: Get the token from the email you received from the researcher.");
-        if(tokenx=="657267"){
+        if(tokenx=="1a2b3c"){
             document.getElementById(id).querySelector("a").setAttribute("href","/static/regulator.html?pt="+serverport+"&em="+respondentemail).click();
         }else if(tokenx!="" && tokenx!=null){
             alert("Incorrect access token! Please enter again.");
         }
     }else if(targetElementId=="proceed-as-donor"){
         var tokenx = prompt("Please enter donor access token: \nHint: Get the token from the email you received from the researcher.");
-        if(tokenx=="6e676f"){
+        if(tokenx=="6d5e4f"){
             document.getElementById(id).querySelector("a").setAttribute("href","/static/donor.html?pt="+serverport+"&em="+respondentemail).click();
         }else if(tokenx!="" && tokenx!=null){
             alert("Incorrect access token! Please enter again.");
         }
     }else if(targetElementId=="proceed-as-ngo"){
         var tokenx = prompt("Please enter ngo access token: \nHint: Get the token from the email you received from the researcher.");
-        if(tokenx=="646f6e"){
+        if(tokenx=="3d4e5f"){
             document.getElementById(id).querySelector("a").setAttribute("href","/static/ngo.html?pt="+serverport+"&em="+respondentemail).click();
         }else if(tokenx!="" && tokenx!=null){
             alert("Incorrect access token! Please enter again.");
